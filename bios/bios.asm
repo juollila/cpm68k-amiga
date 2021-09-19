37937353d2800334c534839b607450a3493c4fca
Sturmmann <macsturm@gmail.com>
2021-09-19 14:36:00 +0300

    init floppy drive control

--- bios/bios.asm
+++ bios/bios.asm
@@ -11,6 +11,7 @@ BLTDPTH	= $054	; Blitter pointer to destination D (high)
 BLTSIZE	= $058	; Blit size (starts blit)
 BLTAMOD	= $064	; Blitter modulo for source A
 BLTDMOD	= $066	; Blitter modulo for destination D
+DSKSYNC	= $07e	; Disk sync pattern
 COP1LCH	= $080	; Copper pointer 1 (high)
 COPJMP1	= $088	; Trigger copper 1 (any value)
 DIWSTRT	= $08e	; Display window start
@@ -20,6 +21,7 @@ DDFSTOP	= $094	; Display bitplane data fetch stop
 DMACON	= $096	; DMA control write
 INTENA	= $09a	; Interrupt enable bits
 INTREQ	= $09c	; Interrupt request bits
+ADKCON	= $09e	; Audio, disk and UART control
 BPL1PTH	= $0e0	; Bitplane pointer 1 (high)
 BPLCON0	= $100	; Bitplane depth and screen mode
 BPLCON1	= $102	; Bitplane/playfield horizontal scroll values
@@ -45,7 +47,7 @@ SECTORSPERTRACK	= 11
 SECTORSIZE	= 512
 TRACKSIZE	= SECTORSPERTRACK*SECTORSIZE
 SECTORS		= TRACKS*SECTORSPERTRACK
-RESERVEDTRACKS	= 6	; 24k (bdos + ccp) + 8k (bios)
+RESERVEDTRACKS	= 6	; boot loader + 24k (bdos + ccp) + 8k (bios)
 BLOCKSIZE	= 2048	; CPM FS block size
 DIRENTRIES	= 128	; CPM directory entries
 DRIVES		= 2	; Only 2 floppy drives supported (Amiga can support upto 4)
@@ -83,109 +85,141 @@ CRB	= $f00	; Control register B
 ;	d0.w: $00
 ; Return value: d0.w: user/disk numbers
 
-_init:
+_init:	
 	lea	CUSTOM,a6
+	bsr	takeover
+	bsr	initscreen
+	bsr	initkeyboard
+	bsr	initfloppy
+
+	lea	biosstring(pc),a0
+	bsr	printstring
+	move.l	#$1234ABCD,d0
+	bsr	printlong
+
+	bra	dosomestuff
+
+takeover:
+	; take over system
 	move.w	#$7fff,INTENA(a6)	; disable interrupts
 	move.w	#$7fff,INTREQ(a6)	; clear interrupts
 	move.w	#$7fff,DMACON(a6)	; disable dma (dmacon)
+	rts
+
+initscreen:
+	; setup screen
 	move.w	#$9200,BPLCON0(a6)	; hires, one bit plane, color enabled
 	move.w	#0,BPLCON1(a6)		; horizontal scroll value = 0
 	move.w	#0,BPL1MOD(a6)		; set modulo 0 for all odd bit planes
-	;move.w	#0,$10a(a6)		; bplmod2: set modulo 0 for all even bit planes
 	move.w	#$3c,DDFSTRT(a6)	; data fetch start for hires
 	move.w	#$d4,DDFSTOP(a6)	; data fetch stop
 	move.w	#$2c81,DIWSTRT(a6)	; display window start
 	move.w	#$2cc1,DIWSTOP(a6)	; display window stop
 	move.w	#$ccc,COLOR0(a6)	; color0: light gray
 	move.w	#$000,COLOR1(a6)	; color1: black
-
+	; init copper list
 	move.l	#screen,d0		; copy screen address to copper list
 	move.w	d0,copper+6
 	swap	d0
 	move.w	d0,copper+2
-
 	move.l	#copper,COP1LCH(a6)	; set copper1 list address
 	clr.w	COPJMP1(a6)		; jump to copper1 list
-	move.w	#$83c0,DMACON(a6)	; dmacon: enable bit-plane, blitter and copper dma
+	; enable bit-plane, blitter and copper dma
+	move.w	#$83c0,DMACON(a6)
+	; clear screen
+	move.w	#$1a,d1
+	bsr	conout
+	rts
 
+initkeyboard:
 	lea	CIAA,a0
 	move.b	#0,CRA(a0)		; disable timer A
 	move.b	#0,CRB(a0)		; disable timer B
-	move.b	#$03,DDRA(a0)		; two low bits are output in port a of CIAA
 	move.b	#$7f,ICR(a0)		; clear all interrupts in CIAA
 	move.b	#$88,ICR(a0)		; enable serial port interrupt in CIAA
-
 	move.b	#$7f,CIAB+ICR		; clear all interrupts in CIAB
-	
 	move.l	#keyboard_int,$68.w	; keyboard interrupt vector to level 2 autovector
 	move.w	#$c008,INTENA(a6)	; enable CIA interrupts
-	
+	rts	
 
-	lea	text,a2
+initfloppy:
+	lea	CIAA,a0
+	lea	CIAB,a1
+	move.b	#$03,DDRA(a0)		; audio filter & rom overlay are outputs
+					; joy0 fire, joy1 fire, drive ready, track 0, write prot & disk change are inputs
+	move.b	#$ff,DDRB(a1)		; motor, sel3, sel2, sel1, sel0, side, dir and step are outputs
+	move.b	#$ff,PRB(a1)		; disable all
+	move.w	#$6200,ADKCON(a6)	; disable precomp1, precomp0 and msbsync
+	move.w	#$9500,ADKCON(a6)	; enable mfmprec, wordsync and fast
+	move.w	#$4489,DSKSYNC(a6)	; set mfm sync mark
+	move.w	#$8010,DMACON(a6)	; enable disk DMA
+	rts
+
+; print string
+;
+; a0 = address of string
+printstring:
+	move.l	a2,-(sp)
+	move.l	a0,a2
 .loop:
 	move.b	(a2)+,d1
-	beq	.skip
+	beq	.exit
 	bsr	conout
 	bra	.loop
+.exit:
+	move.l	(sp)+,a2
+	rts
+
+; print byte
+;
+; d0.b = byte to print
+printbyte:
+	move.l	d0,-(sp)
+	move.b	d0,d1
+	rol.b	#4,d1
+	and.b	#$f,d1
+	lea	hex(pc),a0
+	move.b	0(a0,d1),d1
+	bsr	conout
+	move.l	(sp)+,d0
+	move.l	d0,-(sp)
+	move.b	d0,d1
+	and.b	#$f,d1
+	lea	hex(pc),a0
+	move.b	0(a0,d1),d1
+	bsr	conout
+	move.l	(sp)+,d0
+	rts
+
+; print word as hex
+;
+; d0.w = word to print
+printword:
+	rol.w	#8,d0
+	bsr	printbyte
+	rol.w	#8,d0
+	bsr	printbyte
+	rts
+
+; print long as hex
+;
+; d0.l = long to print
+printlong:
+	swap	d0
+	bsr	printword
+	swap	d0
+	bsr	printword
+	rts
+
+hex:	dc.b	"0123456789ABCDEF"
+
+
+dosomestuff:
 .skip:
 	bsr	conin
 	move.w	d0,d1
 	bsr	conout
 	bra	.skip
-	jmp	*
-text:	;dc.b	"Hello, World!",13,10,"Does this work?",13,10
-	;dc.b	"Very long line:",13,10
-	;dc.b    "1234567890123456789012345678901234567890123456789012345678901234567890"
-	;dc.b    "1234567890123456789012345678901234567890123456789012345678901234567890"
-	;dc.b    "1234567890123456789012345678901234567890123456789012345678901234567890",$1a,0
-	dc.b	"	code_c",13,10,13,10
-	dc.b	"; Function 0: Initialization",13,10
-	dc.b	"; Entry parameters:",13,10
-	dc.b	";	d0.w: $00",13,10
-	dc.b	"; Return value: d0.w: user/disk numbers",13,10
-	dc.b	"_init:",13,10
-	dc.b	"	lea	$dff000,a6",13,10
-	dc.b	"	move.w	#$7fff,$9a(a6)		; disable interrupts (intena)",13,10
-	dc.b	"	move.w	#$7fff,$9c(a6)		; clear interrupts (intreq)",13,10
-	dc.b	"	move.w	#$7fff,$96(a6)		; disable dma (dmacon)",13,10
-	dc.b	"	move.w	#$9200,$100(a6)		; bplcon0: hires, one bit plane, color enabled",13,10
-	dc.b	"	move.w	#0,$102(a6)		; bplcon1: horizontal scroll value = 0",13,10
-	dc.b	"	move.w	#0,$108(a6)		; bplmod1: set modulo 0 for all odd bit planes",13,10
-	dc.b	"	move.w	#0,$10a(a6)		; bplmod2: set modulo 0 for all even bit planes",13,10
-	dc.b	"	move.w	#$3c,$92(a6)		; ddfstrt: data fetch start for hires",13,10
-	dc.b	"	move.w	#$d4,$94(a6)		; ddfstop: data fetch stop",13,10
-	dc.b	"	move.w	#$2c81,$8e(a6)		; diwstrt: display window start",13,10
-	dc.b	"	move.w	#$2cc1,$90(a6)		; diwstop: display window stop",13,10
-	dc.b	"	move.w	#$ccc,$180(a6)		; color0: light gray",13,10
-	dc.b	"	move.w	#$000,$182(a6)		; color1: black",13,10,13,10
-
-	dc.b	"	move.l	#screen,d0		; set copper list",13,10,13,10
-	dc.b	"	move.w	d0,copper+6",13,10
-	dc.b	"	swap	d0",13,10
-	dc.b	"	move.w	d0,copper+2",13,10
-
-	dc.b	"	move.l	#copper,$80(a6)		; set copper list address",13,10
-	dc.b	"	move.w	$88(a6),d0		; jump to copper 1 list",13,10
-	dc.b	"	move.w	#$83c0,$96(a6)		; dmacon: enable bit-plane, blitter and copper dma",13,10,13,10
-
-	dc.b	"	lea	text,a2",13,10
-	dc.b	".loop:",13,10
-	dc.b	"	move.b	(a2)+,d1",13,10
-	dc.b	"	beq	.skip",13,10
-	dc.b	"	bsr	conout",13,10
-	dc.b	"	bra	.loop",13,10
-	dc.b	".skip:",13,10
-	dc.b	"	jmp	*",13,10
-	;dc.b	$1b,"=  ","testing",0
-	dc.b	"yksi",8,8,8,8,8,"kaksi",11,"kolme",13,"nelja",11,"viisi",0
-
-;	dc.b	"1",13,"2",13,"3",13,"4",13,"5",13,"6",13,"7",13,"8",13,"9",13,"10",13
-;	dc.b	"11",13,"12",13,"13",13,"14",13,"15",13,"16",13,"17",13,"18",13,"19",13,"20",13
-;	dc.b	"21",13,"22",13,"23",13,"24",13,"25",13,"26",13,"27",13,"28",13,"29",13,"30",13
-;	dc.b	"31",13,"32",13,"33",13,"34",13,"35",13,"36",13,"37",13,"38",13,"39",13,"40",13,0
-	even
-
-
 
 	; TODO: setting up trap handler, internal variables (e.g. iobyte)
 	; move.l	#traphandler,$8c	; set trap 3 handler
@@ -193,9 +227,9 @@ text:	;dc.b	"Hello, World!",13,10,"Does this work?",13,10
 	rts
 
 waitblit:
-	lea	$dff000,a0
+	tst.w	CUSTOM+DMACONR
 .waitblit0:
-	btst	#14,$2(a0)	; wait blit to complete (dmaconr)
+	btst	#14-8,CUSTOM+DMACONR	; wait blit to complete
 	bne	.waitblit0
 	rts 
 
@@ -274,6 +308,7 @@ conout:
 	add.l	d1,d0
 	add.l	#screen,d0
 	movea.l	d0,a1
+	bsr	waitblit	; we don't want mess up with screen if screen blit is ongoing
 	; copy character to screen
 	moveq	#8,d0
 .loop:	move.b	(a0),(a1)
@@ -379,21 +414,17 @@ conout:
 
 	; clear screen
 .cls:
-	jsr	waitblit
+	bsr	waitblit
 	move.l	#screen,BLTDPTH(a0)		; destination address (bltdpth)
 	move.w	#0,BLTDMOD(a0)			; zero modulo (bltdmod)
 	move.w	#0,BLTCON1(a0)			; bltcon1
 	move.w	#$100,BLTCON0(a0)		; use D, minterm = none (bltcon0)
 	move.w	#((256<<6)|(80/2)),BLTSIZE(a0)	; height = 256, width = 40 words
-;.waitmouse:
-;	btst	#6,$bfe001
-;	bne	.waitmouse
-;	move.w	#$ffff,screen
 	rts
 
 	; scroll screen buffer
 .scroll:
-	jsr	waitblit
+	bsr	waitblit
 	move.w	#31,.row
 	move.l	#screen,d0
 	move.l	d0,BLTDPTH(a0)	; destination address (bltdpth)
@@ -406,7 +437,7 @@ conout:
 	move.w	#0,BLTCON1(a0)	; bltcon1
 	move.w	#$9f0,BLTCON0(a0)		; use A&D, minterm = A (bltcon0)
 	move.w	#((248<<6)|(80/2)),BLTSIZE(a0)	; height = 248, width = 40 words
-	jsr	waitblit
+	bsr	waitblit
 	move.l	#screen,d0
 	add.l	#248*80,d0
 	move.l	d0,BLTDPTH(a0)			; destination address (bltdpth)
@@ -414,7 +445,7 @@ conout:
 	move.w	#0,BLTCON1(a0)			; bltcon1
 	move.w	#$100,BLTCON0(a0)		; use D, minterm = none (bltcon0)
 	move.w	#((8<<6)|(80/2)),BLTSIZE(a0)	; height = 8, width = 40 words
-	jsr	waitblit
+	bsr	waitblit
 	;move.w	#$ffff,screen
 	rts
 
@@ -620,16 +651,30 @@ settrack:
 	move.w	d1,floppy_track
 .exit:
 	rts
-	
 
-
-
+; Function 11: Set logical sector number
+;
+; Entry parameters:
+;	d0.w: $0b
+;	d1.w: Logical sector number
+; Return value: None
+setsector:
+	move.w	d1,d0
+	bmi	.exit
+	lsr.w	#2,d0		; divide by 4, one sector contains 4 CP/M logical sectors
+	cmp.w	#SECTORS,d0
+	bcc	.exit
+	move.w	d0,floppy_sector
+	move.w	d1,floppy_logical
+.exit:
+	rts
 
 ; copper list
 copper:
 	dc.w	$e0,0
 	dc.w	$e2,0
 	dc.w	$ffff,$fffe
+
 color:	dc.w	$00f
 
 ; keyboard variables
@@ -645,6 +690,52 @@ caps_lock:
 	dc.w	0
 ctrl_key:
 	dc.w	0
+
+; floppy variables
+floppy_track:
+	dc.w	0
+floppy_drive:
+	dc.w	0
+floppy_sector:
+	dc.w	0
+floppy_logical:
+	dc.w	0
+floppy_dph:
+	dc.l	0			; xlt, no sector translation table
+	dc.w	0			; scratch pad
+	dc.w	0			; scratch pad
+	dc.w	0			; scratch pad
+	dc.l	floppy_dir_buffer	; address of directory buffer
+	dc.l	floppy_dpb		; address of disk parameter block
+	dc.l	floppy_csv		; address of checksum vector
+	dc.l	floppy_alv		; address of scratchpad area
+	
+; floppy disk parameter block
+floppy_dpb:
+	dc.l	TRACKSIZE/128			; spt, 11*512/128=44, number of 128 byte logical sectors per track
+	dc.b	4 				; bsh, block shift factor = 5 when block size = 2048
+	dc.b	15 				; blm, block mask = 15 when block size = 2048
+	dc.b	0				; exm, extent mask = 0 when block size = 2048 and dsm > 255
+	dc.b	0				; reserved
+	dc.w	TRACKS*TRACKSIZE/BLOCKSIZE-1	; dsm, number of last block (2*80)*(11*512)/2048-1 = 439
+	dc.w	DIRENTRIES-1			; drm, directory entries - 1
+	dc.w	0				; reserved, directory mask?
+	dc.w	DIRENTRIES/4			; cks, size of directory check vector (drm/4)+1
+	dc.w	RESERVEDTRACKS			; off, number of reserved tracks
+; floppy directory buffer
+floppy_dir_buffer
+	blk.b	128,0
+; floppy checksum vector
+floppy_csv:
+	blk.b	DIRENTRIES/4,0
+; floppy scratchpad area
+floppy_alv:
+	blk.b	TRACKS*TRACKSIZE/BLOCKSIZE/8
+
+; strings
+biosstring:
+	dc.b	"CP/M-68k BIOS - Coded by Juha Ollila 2021",13,10,0
+	even
 
 ; keymap (raw code to ascii)
 keymap:
@@ -667,42 +758,6 @@ font:
 	incbin ../font/topaz128x112x1.raw
 	even
 
-; floppy variables
-floppy_track:
-	dc.w	0
-floppy_drive:
-	dc.w	0
-floppy_dph:
-	dc.l	0			; xlt, no sector translation table
-	dc.w	0			; scratch pad
-	dc.w	0			; scratch pad
-	dc.w	0			; scratch pad
-	dc.l	floppy_dir_buffer	; address of directory buffer
-	dc.l	floppy_dpb		; address of disk parameter block
-	dc.l	floppy_csv		; address of checksum vector
-	dc.l	floppy_alv		; address of scratchpad area
-	
-; floppy disk parameter block
-floppy_dpb:
-	dc.l	TRACKSIZE/128			; spt, 11*512/128=44, number of 128 byte logical sectors per track
-	dc.b	4 				; bsh, block shift factor = 5 when block size = 2048
-	dc.b	15 				; blm, block mask = 15 when block size = 2048
-	dc.b	0				; exm, extent mask = 0 when block size = 2048 and dsm > 255
-	dc.b	0				; reserved
-	dc.w	TRACKS*TRACKSIZE/BLOCKSIZE-1	; dsm, number of last block 160*11*512/2048-1 = 439
-	dc.w	DIRENTRIES-1			; drm, directory entries - 1
-	dc.w	0				; reserved, directory mask?
-	dc.w	DIRENTRIES/4			; cks, size of directory check vector (drm/4)+1
-	dc.w	RESERVEDTRACKS			; off, number of reserved tracks
-; floppy directory buffer
-floppy_dir_buffer
-	blk.b	128,0
-; floppy checksum vector
-floppy_csv:
-	blk.b	DIRENTRIES/4,0
-; floppy scratchpad area
-floppy_alv:
-	blk.b	TRACKS*TRACKSIZE/BLOCKSIZE/8
-; screen buffer
+; screen buffer (requires 20kB)
 screen:
 	;blk.b	640*256*1/8, 0
