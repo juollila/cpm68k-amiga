c07cf7bf2d9ce54f496348d5ef56efcd5b46b92d
Juha Ollila <macsturm@gmail.com>
2026-08-09 13:19:18 +0300

    move utility functions to util.cow in startrek game

--- amiga/extras/startrek/build.sh
+++ amiga/extras/startrek/build.sh
@@ -5,11 +5,17 @@ fi
 if [ -f "starmain.cob" ] ; then
    rm "starmain.cob"
 fi
+if [ -f "util.cob" ] ; then
+   rm "util.cob"
+fi
 if [ -f "startrek.coo" ] ; then
    rm "startrek.coo"
 fi
 if [ -f "starmain.coo" ] ; then
    rm "starmain.coo"
+fi
+if [ -f "util.coo" ] ; then
+   rm "util.coo"
 fi
 if [ -f "startrek.s" ] ; then
    rm "startrek.s"
@@ -25,7 +31,9 @@ cowfe-for-32bita2-with-nncgen -I$COWGOLPATH/rt/ -I$COWGOLPATH/rt/amigacpm/ start
 cowbe-for-68000-with-nncgen startrek.cob startrek.coo
 cowfe-for-32bita2-with-nncgen -I$COWGOLPATH/rt/ -I$COWGOLPATH/rt/amigacpm/ starmain.cow starmain.cob
 cowbe-for-68000-with-nncgen starmain.cob starmain.coo
-cowlink-for-amigacpm-with-nncgen $COWGOLPATH/.obj/rt/amigacpm/+cowgolcoo/cowgol.coo startrek.coo starmain.coo -o startrek.s
+cowfe-for-32bita2-with-nncgen -I$COWGOLPATH/rt/ -I$COWGOLPATH/rt/amigacpm/ util.cow util.cob
+cowbe-for-68000-with-nncgen util.cob util.coo
+cowlink-for-amigacpm-with-nncgen $COWGOLPATH/.obj/rt/amigacpm/+cowgolcoo/cowgol.coo startrek.coo util.coo starmain.coo -o startrek.s
 m68k-atari-mint-as startrek.s -o startrek.o 
 m68k-atari-mint-ld -T $COWGOLPATH/third_party/amigacpm/amigacpm.ld -o startrek.68k startrek.o
 ls -l startrek.68k
